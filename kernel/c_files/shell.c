cb123a84fd89e7c96469b49a098a8dae878b425f
WqVoon <2027195958@qq.com>
2021-03-04 19:11:12 +0800

    添加了 shell 命令解析函数 cmd_parse

--- kernel/c_files/shell.c
+++ kernel/c_files/shell.c
@@ -45,6 +45,48 @@ static void readline(char* buf, int32_t count) {
 	printf("[ERROR] out of max cmd len\n");
 }
 
+/* 分析字符串 cmd_str 中以 token 为分隔符的单词，并将其指针存入 argv 数组 */
+static int32_t cmd_parse(char* cmd_str, char** argv, char token) {
+	int32_t arg_idx = 0;
+	while (arg_idx < MAX_ARG_NR) {
+		argv[arg_idx] = NULL;
+		arg_idx++;
+	}
+
+	char* next = cmd_str;
+	int32_t argc = 0;
+	while (*next) {
+		// 跳过所有的 token
+		while (*next == token) {
+			next++;
+		}
+
+		// 处理命令最后带 token 的情况
+		if (*next == 0) {
+			break;
+		}
+		argv[argc] = next;
+
+		// 开始处理一个单词，扫描直到遇到 \0 或者 token
+		while (*next && *next != token) {
+			next++;
+		}
+		// 分割字符串
+		*next = 0;
+
+		if (argc > MAX_ARG_NR) {
+			return -1;
+		}
+
+		next++;
+		argc++;
+	}
+	return argc;
+}
+
+char* argv[MAX_ARG_NR];
+int32_t argc = -1;
+
 /* 简单的 shell */
 void my_shell(void) {
 	while (1) {
@@ -55,5 +97,17 @@ void my_shell(void) {
 		if (cmd_line[0] == 0) {
 			continue;
 		}
+		argc = cmd_parse(cmd_line, argv, ' ');
+		if (argc == -1) {
+			printf("[ERROR] out of max number of argv");
+			continue;
+		}
+
+		int32_t arg_idx = 0;
+		while (arg_idx < argc) {
+			printf("%s ", argv[arg_idx]);
+			arg_idx++;
+		}
+		putchar('\n');
 	}
 }
